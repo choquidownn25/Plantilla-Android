9229b7cfd3b6f38d235aa0614a6e4ae21bc45f85
Choquidownn25 <jose_sarria2003@yahoo.com.mx>
2020-01-13 11:36:11 -0500

    Maps para uber
    Otra capa de base para uber

---
+++ app/src/main/java/com/example/plantilla/uber/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.example.plantilla.uber;
+
+import com.example.plantilla.uber.models.Result;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by Amanjeet Singh on 8/9/18.
+ */
+public interface ApiInterface {
+
+    @GET("maps/api/directions/json")
+    Single<Result> getDirections(@Query("mode") String mode,
+                                 @Query("transit_routing_preference") String routingPreference,
+                                 @Query("origin") String origin,
+                                 @Query("destination") String destination,
+                                 @Query("key") String apiKey);
+}
---
+++ app/src/main/java/com/example/plantilla/uber/models/events/BeginJourneyEvent.java
@@ -0,0 +1,21 @@
+package com.example.plantilla.uber.models.events;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Amanjeet Singh on 9/9/18.
+ */
+public class BeginJourneyEvent {
+
+    private String event = "BEGIN_JOURNEY";
+    private LatLng beginLatLng;
+
+    public LatLng getBeginLatLng() {
+        return beginLatLng;
+    }
+
+    public void setBeginLatLng(LatLng beginLatLng) {
+        this.beginLatLng = beginLatLng;
+    }
+
+}
